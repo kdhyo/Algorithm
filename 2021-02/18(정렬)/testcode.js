fbcb8c081b5bf39da4f07072c7578e57ce420f1d
hyo <kdhyo98@gmail.com>
2021-02-20 00:30:00 +0900

    다른사람 testcode추가

--- 2021-02/18(정렬)/testcode.js
+++ 2021-02/18(정렬)/testcode.js
@@ -12,3 +12,17 @@ const array = [1,5,2,6,3,7,4];
 const commands = [[2,5,3], [4,4,1], [1,7,3]];
 
 console.log(solution(array, commands)); //기대값 [5, 6, 3]
+
+
+// 다른 사람이 작성한 내가 마음에 들었던 코드
+// [sPosition, ePosition, position] = command 이 부분이 직관적이어서 가장 마음에 들었다.
+function solution(array, commands) {
+  return commands.map(command => {
+      const [sPosition, ePosition, position] = command
+      const newArray = array
+          .filter((value, fIndex) => fIndex >= sPosition - 1 && fIndex <= ePosition - 1)
+          .sort((a,b) => a - b)    
+
+      return newArray[position - 1]
+  })
+}
