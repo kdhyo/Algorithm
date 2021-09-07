525371edec4044306d2c7d0a1ebda0c427a88188
kdhyo <kdhyo98@gmail.com>
2021-09-07 21:54:06 +0900

    [09/07] 체육복 - 집가서 풀이확인

--- 2021-09/체육복.js
+++ 2021-09/체육복.js
@@ -1,4 +1,6 @@
 // 뭐가문제지
+// https://velog.io/@grappe96/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EA%B3%A0%EB%93%9D%EC%A0%90-Kit-%EC%B2%B4%EC%9C%A1%EB%B3%B5-%EB%AC%B8%EC%A0%9C-%ED%92%80%EC%9D%B4-JS
+// 위 코드함봐야지
 
 function solution(n, lost, reserve) {
     lost.sort((a, b) => a - b);
