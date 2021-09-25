77554dc7a76090dbd86e6c6c20874bd25fdb28ea
김동효 <kdhyo98@gmail.com>
2021-09-25 18:47:18 +0900

    [09/25] 예제추가

--- 2021-09/숫자문자열과영단어.js
+++ 2021-09/숫자문자열과영단어.js
@@ -1,12 +1,16 @@
 function solution(s) {
     const numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-    const result = [];
-    let check = [];
-    
+
     for(let i = 0; i < 10; i++) {
         while(s.match(numbers[i])) {
             s = s.replace(numbers[i], i);
         }
     }
+
     return +s;
 }
+
+console.log(solution("one4seveneight")); //1478
+console.log(solution("23four5six7")); //234567
+console.log(solution("2three45sixseven")); //234567
+console.log(solution("123")); //123
