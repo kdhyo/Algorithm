2f46fb566f840c4b827a3d054e365eb68cf77983
김동효 <kdhyo98@gmail.com>
2021-09-25 18:50:51 +0900

    [09/25] 다른사람 풀이 추가

--- 2021-09/숫자문자열과영단어.js
+++ 2021-09/숫자문자열과영단어.js
@@ -14,3 +14,19 @@ console.log(solution("one4seveneight")); //1478
 console.log(solution("23four5six7")); //234567
 console.log(solution("2three45sixseven")); //234567
 console.log(solution("123")); //123
+
+
+/* 다른 사람이 짠 배우고 싶은 코드 */
+function solution(s) {
+    s = s.replace(/zero/gi, 0)
+    .replace(/one/gi, 1)
+    .replace(/two/gi, 2)
+    .replace(/three/gi, 3)
+    .replace(/four/gi, 4)
+    .replace(/five/gi, 5)
+    .replace(/six/gi, 6)
+    .replace(/seven/gi, 7)
+    .replace(/eight/gi, 8)
+    .replace(/nine/gi, 9)
+    return parseInt(s);
+}
