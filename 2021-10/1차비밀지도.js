fc0f4af579750361354db0787dc27eec3c3e5ef8
김동효 <kdhyo98@gmail.com>
2021-10-01 01:16:19 +0900

    [10/01] 1차비밀지도 수정

--- 2021-10/1차비밀지도.js
+++ 2021-10/1차비밀지도.js
@@ -23,3 +23,13 @@ arr1	[9, 20, 28, 18, 11]
 arr2	[30, 1, 21, 17, 28]
 출력	["#####","# # #", "### #", "# ##", "#####"]
 */
+
+// 다른사람 풀이보고 변경한 코드
+// 비트연산이랑 replace를 생각하게 됐다.
+function solution(n, arr1, arr2) {
+    const answer = [];
+    return arr1.map((num, idx) => (num | arr2[idx])
+             .toString(2).padStart(n, '0')
+             .replace(/1/g, "#")
+             .replace(/0/g, " "));
+}
