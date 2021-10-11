2de7cfa12f144fe0668f6bb41cbb3de3560792b0
김동효 <kdhyo98@gmail.com>
2021-10-11 23:57:06 +0900

    [10/11] 인프런 보이는학생 성공

---
+++ inflearnCodingTest/2차원배열/보이는학생.js
@@ -0,0 +1,26 @@
+/* 내 풀이 */
+function solution(arr) {
+    let answer = 1, max = arr[0];
+    for(let i = 1; i < arr.length; i++) {
+        if(arr[i] > max) {
+            max = arr[i];
+            answer++;
+        }
+    }
+    return answer;
+}
+
+/* 해설 풀이 */
+function solution2(arr){         
+    let answer=1, max=arr[0];
+    for(let i=1; i<arr.length; i++){
+        if(arr[i]>max){
+            answer++;
+            max=arr[i];
+        }
+    }
+    return answer;
+}
+
+let arr=[130, 135, 148, 140, 145, 150, 150, 153];
+console.log(solution(arr));
