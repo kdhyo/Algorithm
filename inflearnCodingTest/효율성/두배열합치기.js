643980e3511dc6722fca447d42e2e1a7edc60673
김동효 <kdhyo98@gmail.com>
2021-10-15 16:13:54 +0900

    [10/15] 인프런 두배열합치기 성공

---
+++ inflearnCodingTest/효율성/두배열합치기.js
@@ -0,0 +1,24 @@
+/* 내 풀이 */
+function solution(arr1, arr2) {
+    return [...arr1, ...arr2].sort((a, b) => a - b);
+}
+
+
+/* 해설 풀이 */
+function solution2(arr1, arr2){
+    let answer=[];
+    let n=arr1.length;
+    let m=arr2.length;
+    let p1=p2=0;
+    while(p1<n && p2<m){
+        if(arr1[p1]<=arr2[p2]) answer.push(arr1[p1++]);
+        else answer.push(arr2[p2++]);
+    }
+    while(p1<n) answer.push(arr1[p1++]);
+    while(p2<m) answer.push(arr2[p2++]); 
+    return answer;
+}
+
+let a=[1, 3, 5];
+let b=[2, 3, 6, 7, 9];
+console.log(solution(a, b));
